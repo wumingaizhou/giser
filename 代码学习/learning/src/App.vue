cb3f6e92edb236193df0170411a320f61dc82bf3
wu-ming211105 <1535782250@qq.com>
2024-08-04 18:00:54 +0800

    测试

--- 代码学习/learning/src/App.vue
+++ 代码学习/learning/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>一张图地图</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container class="mapContainer">
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -23,6 +29,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-header {
+  background-color: #909399;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  border-radius: 5px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+  height: 100vh;
+  border-radius: 5px;
+}
+
+.mapContainer {
+  height: 100vh;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  height: 100%;
 }
 </style>
